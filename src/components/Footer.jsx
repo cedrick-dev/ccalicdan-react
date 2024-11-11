0d088913b2f37458bdfe33720151b8a3ffe4a972
cedrick-dev <ccalicdan23@gmail.com>
2024-11-11 23:42:15 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="max-w-[1200px] sm:h0[150px] p-12 flex justify-between mx-auto">
+    <div className="max-w-[2000px] sm:h0[150px] p-12 flex justify-between mx-auto">
         <span className="text-white">C. Calicdan</span>
         <p className="text-gray-600">ccalicdan23@gmail.com<br/> #webDev2024</p>
          
